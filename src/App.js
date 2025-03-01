b4ef06ff0399ba1e406df71f46fefe1a69321e2d
unknown <mslearnkeshav@outlook.com>
2025-03-01 20:48:59 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SearchBar from './Components/SearchBar';
+import searchImages from './api';
+import { useState } from 'react';
+import ImageList from './Components/ImageList';
+function App(){
+    const[images,setImages]=useState([]);
+    const handleSubmit =async(term)=>{
+       const result= await searchImages(term);
+       setImages(result);
+    }
+    return (
+        <div>
+           <SearchBar onSubmit={handleSubmit} />
+           <ImageList images={images} />
+        </div>
+    )
 }
-
 export default App;
